9ac9701d7f86373c638451e3748f9ca275d13b1b
xinyao240 <65817343+xinyao240@users.noreply.github.com>
2020-05-23 22:37:48 +0800

    数据库模型文件

---
+++ accounttablemodel.h
@@ -0,0 +1,23 @@
+/**This class is only provided for reader and administrator
+ * model with common behaviours such as the change of
+ * name and passward in user interface.
+ * */
+
+#ifndef ACCOUNTTABLEMODEL_H
+#define ACCOUNTTABLEMODEL_H
+
+#include <QtSql>
+
+class AccountTableModel : public QSqlTableModel
+{
+    Q_OBJECT
+public:
+    AccountTableModel(const QString &type, QObject *parent = nullptr);
+    int addEntry(const QString &name, const QString &password); // add an entry and returns the id
+
+public slots:
+    void changeName(int id, const QString &name);
+    void changePassword(int id, const QString &password);
+};
+
+#endif // ACCOUNTTABLEMODEL_H
