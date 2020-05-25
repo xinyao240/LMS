cd9470cc3ca9787df1ed1cbcddcdafaea4410062
yioneko <65551246+yioneko@users.noreply.github.com>
2020-05-25 22:12:47 +0800

    Initial commit
    Database initialization and primary login/register widgets design.
    NOTE: Book genres not provided.

---
+++ registerwidget.cpp
@@ -0,0 +1,124 @@
+#include "registerwidget.h"
+#include "loginwidget.h"
+#include "constants.h"
+
+RegisterWidget::RegisterWidget(QWidget *parent, QSharedPointer<AccountTableModel> readerTable)
+    : QDialog(parent), readerTable(readerTable)
+{
+    QLabel *nameLabel = new QLabel(tr("姓名"), this);
+    QLabel *passwordLabel = new QLabel(tr("密码"), this);
+    QLabel *reinputLabel = new QLabel(tr("重复密码"), this);
+
+    nameEdit = new QLineEdit(this);
+    nameEdit->setPlaceholderText(tr("输入姓名"));
+    // To do validator 目前是想实现首尾无空格
+    nameEdit->setMaxLength(MAX_NAME_LENGTH);
+
+    passwordEdit = new QLineEdit(this);
+    passwordEdit->setPlaceholderText(tr("输入密码"));
+    passwordEdit->setAttribute(Qt::WA_InputMethodEnabled, false);
+    passwordEdit->setEchoMode(QLineEdit::Password);
+    passwordEdit->setValidator(new QRegExpValidator(QRegExp("[\\S]*"), passwordEdit));
+    passwordEdit->setMaxLength(MAX_PASSWORD_LENGTH);
+
+    reinputEdit = new QLineEdit(this);
+    reinputEdit->setPlaceholderText(tr("重复输入密码"));
+    reinputEdit->setAttribute(Qt::WA_InputMethodEnabled, false);
+    reinputEdit->setEchoMode(QLineEdit::Password);
+    reinputEdit->setValidator(new QRegExpValidator(QRegExp("[\\S]*"), passwordEdit));
+    reinputEdit->setMaxLength(MAX_PASSWORD_LENGTH);
+
+    registerButton = new QPushButton(tr("确认注册"), this);
+    registerButton->setDisabled(true);
+    QPushButton *returnButton = new QPushButton(tr("返回登录"), this);
+
+    QGridLayout *editLayout = new QGridLayout;
+    editLayout->addWidget(nameLabel, 0, 0);
+    editLayout->addWidget(nameEdit, 0, 1);
+    editLayout->addWidget(passwordLabel, 1, 0);
+    editLayout->addWidget(passwordEdit, 1, 1);
+    editLayout->addWidget(reinputLabel, 2, 0);
+    editLayout->addWidget(reinputEdit, 2, 1);
+
+    QHBoxLayout *buttonLayout = new QHBoxLayout;
+    buttonLayout->addWidget(returnButton, 0, Qt::AlignTop);
+    buttonLayout->addWidget(registerButton, 0, Qt::AlignTop);
+
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+    mainLayout->addLayout(editLayout);
+    mainLayout->addLayout(buttonLayout);
+    // mainLayout->setSizeConstraint(QLayout::SetFixedSize);
+    // 使对话框不可放缩
+
+    setLayout(mainLayout);
+    setWindowTitle(tr("注册"));
+    resize(INITIAL_DIALOG_SIZE);
+
+    timer = new QTimer(this);
+    timer->setInterval(100);
+
+    registerMessage = new QMessageBox(QMessageBox::Information, tr("注册成功"), "", QMessageBox::Ok);
+    registerMessage->setModal(true);
+
+    connect(timer, &QTimer::timeout, this, &RegisterWidget::updateRegisterButton);
+    connect(registerButton, &QPushButton::clicked, this, &RegisterWidget::judgeRegister);
+    connect(returnButton, &QPushButton::clicked, this, &RegisterWidget::clearAndBack);
+    connect(this, &RegisterWidget::accountCreated, dynamic_cast<LoginWidget *>(parent), &LoginWidget::autoFill);
+    connect(registerMessage, &QMessageBox::accepted, this, &RegisterWidget::clearAndBack);
+}
+
+//restart timer after shown
+//在展示后重新计时
+void RegisterWidget::showEvent(QShowEvent * event)
+{
+    QWidget::showEvent(event);
+    timer->start();
+}
+
+//set register button enabled while all line editor have text
+//仅当所有输入框都有文本时才启用按钮
+void RegisterWidget::updateRegisterButton()
+{
+    if (!nameEdit->text().isEmpty() && !passwordEdit->text().isEmpty()
+            && !reinputEdit->text().isEmpty())
+        registerButton->setEnabled(true);
+    else
+        registerButton->setDisabled(true);
+}
+
+void RegisterWidget::judgeRegister()
+{
+    const QString name = nameEdit->text();
+    const QString password = passwordEdit->text();
+    const QString reinput = reinputEdit->text();
+
+    if (password.length() < MIN_PASSWORD_LENGTH) {
+        QMessageBox::warning(this, tr("密码长度过短"), tr("请输入至少6位密码以保证安全性"));
+        return;
+    }
+
+    if (password != reinput) {
+        QMessageBox::warning(this, tr("两次密码输入不一致"), tr("请检查两次输入的密码是否一致"));
+        return;
+    }
+
+    int newId = readerTable->addEntry(name, password);
+
+    if (newId == -1) {
+        QMessageBox::critical(this, tr("程序出错"), tr("注册失败。请联系管理员或开发者获取支持。"));
+        return;
+    }
+    emit accountCreated(newId, password);
+    registerMessage->setText(tr("您的ID是：%1").arg(newId));
+    registerMessage->show();
+}
+
+void RegisterWidget::clearAndBack()
+{
+    timer->stop(); //停止计时
+    nameEdit->clear();
+    passwordEdit->clear();
+    reinputEdit->clear();
+    this->hide();
+    dynamic_cast<LoginWidget *>(parent())->show();
+}
---
+++ untitled3_zh_CN.ts
@@ -0,0 +1,153 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="untitled3_zh_CN">
+<context>
+    <name>LoginWidget</name>
+    <message>
+        <location filename="loginwidget.cpp" line="11"/>
+        <source>欢迎</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="loginwidget.cpp" line="12"/>
+        <location filename="loginwidget.cpp" line="29"/>
+        <source>账号ID</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="loginwidget.cpp" line="13"/>
+        <location filename="loginwidget.cpp" line="36"/>
+        <source>密码</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="loginwidget.cpp" line="18"/>
+        <source>读者登录</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="loginwidget.cpp" line="19"/>
+        <source>管理员登录</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="loginwidget.cpp" line="43"/>
+        <location filename="loginwidget.cpp" line="70"/>
+        <source>登录</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="loginwidget.cpp" line="44"/>
+        <source>注册</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="loginwidget.cpp" line="85"/>
+        <source>未输入ID</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="loginwidget.cpp" line="85"/>
+        <source>请输入ID</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="loginwidget.cpp" line="89"/>
+        <source>未输入密码</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="loginwidget.cpp" line="89"/>
+        <source>请输入密码</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="loginwidget.cpp" line="97"/>
+        <source>ID不存在</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="loginwidget.cpp" line="97"/>
+        <source>请重新输入ID</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="loginwidget.cpp" line="112"/>
+        <source>密码错误</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="loginwidget.cpp" line="112"/>
+        <source>请重新输入密码</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RegisterWidget</name>
+    <message>
+        <location filename="registerwidget.cpp" line="8"/>
+        <source>姓名</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="registerwidget.cpp" line="9"/>
+        <source>密码</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="registerwidget.cpp" line="10"/>
+        <source>重复密码</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="registerwidget.cpp" line="13"/>
+        <source>输入姓名</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="registerwidget.cpp" line="18"/>
+        <source>输入密码</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="registerwidget.cpp" line="25"/>
+        <source>重复输入密码</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="registerwidget.cpp" line="31"/>
+        <source>确认注册</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="registerwidget.cpp" line="33"/>
+        <source>返回登录</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="registerwidget.cpp" line="54"/>
+        <source>注册</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="registerwidget.cpp" line="60"/>
+        <source>注册成功</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="registerwidget.cpp" line="96"/>
+        <source>两次密码输入不一致</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="registerwidget.cpp" line="96"/>
+        <source>请检查两次输入的密码是否一致</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="registerwidget.cpp" line="101"/>
+        <source>您的ID是：%1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
