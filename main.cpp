f146b97cc216aba7dd0d425aae59d0b716b7d531
xinyao240 <65817343+xinyao240@users.noreply.github.com>
2020-06-07 11:04:20 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,12 +1,46 @@
-#include "mainwindow.h"
+
 #include "loginwidget.h"
 
 #include <QApplication>
-#include <QtSql>
+#include <QSharedPointer>
+#include "initdb.h"
+#include "accounttablemodel.h"
+#include "loginwidget.h"
+#include "admininterface.h"
+
+initDb ini;
+
+LoginWidget *loginWidget;
+UserInterface *mainInterface;
+
+void initial();
+void clear();
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    return a.exec();
+    a.setApplicationName("图书管理系统");
+    initial();
+
+    loginWidget->show();
+    mainInterface->show();
+    if(a.exec()){
+        clear();
+    }
+    return 0;
+}
+void initial(){
+    QSqlError error = ini();
+    if (error.type() != QSqlError::NoError) {
+        qWarning() <<  "Error in SQL initialization: " << error;
+        // 建议在需要输出调试/错误消息时这样写，方便后面进行日志的格式化与管理
+    }
+    QSharedPointer<AccountTableModel> readerTable = (QSharedPointer<AccountTableModel>)new AccountTableModel("reader");
+    QSharedPointer<AccountTableModel> adminTable = (QSharedPointer<AccountTableModel>)new AccountTableModel("admin");
+
+    loginWidget = new LoginWidget(readerTable, adminTable);
+    mainInterface = new AdminInterface(0,"dd");
+}
+void clear(){
+    delete loginWidget;
 }
